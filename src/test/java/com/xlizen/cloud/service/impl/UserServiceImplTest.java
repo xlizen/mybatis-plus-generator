041fc26a1b1e075b3126dc6dac197c2df6307485
xlizen <Leng,!!8>
2020-04-06 22:43:05 +0800

    backup

---
+++ src/test/java/com/xlizen/cloud/service/impl/UserServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.xlizen.cloud.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+    @Test
+    void getUserInfoByName() {
+    }
+}
